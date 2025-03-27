9344c318ca1ccd82314e2368370686b151333492
Isivaleriano <126919136+Isivaleriano@users.noreply.github.com>
2025-03-27 10:47:14 -0300

    Update sim.c

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include "shell.h"
+#include "decode.h"
+#include "execute.h"
 
-void process_instruction()
-{
-    /* execute one instruction here. You should use CURRENT_STATE and modify
-     * values in NEXT_STATE. You can call mem_read_32() and mem_write_32() to
-     * access memory. 
-     * */
+
+void process_instruction() {
+    uint32_t instruction_hex = mem_read_32(CURRENT_STATE.PC);
+    Instruction inst = decode_instruction(instruction_hex);
+    execute_instruction(inst);
+
+    NEXT_STATE.PC = CURRENT_STATE.PC + 4;
 }

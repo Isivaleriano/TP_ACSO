0652310bd47b108dd6bcccd11d7dad82d1ce5e16
PauCacchiotti <pcacchiotti@udesa.edu.ar>
2025-03-30 21:12:38 -0300

    hice cambios

--- TP1-ARM/inputs/ldur_test.s
+++
@@ -1,5 +0,0 @@
-movz X1, #0xABCD           // Valor a guardar
-movz X2, #0x1000, lsl #16  // Dirección base: 0x10000000
-stur X1, [X2, #0x10]       // Guardamos en memoria
-ldur X3, [X2, #0x10]       // Cargamos en X3
-hlt 0
--- TP1-ARM/inputs/ldurb_test.s
+++
@@ -1,6 +0,0 @@
-.text
-movz X1, #0xAB                // X1 = 0xAB
-movz X2, #0x1000, lsl #16     // X2 = 0x10000000
-stur X1, [X2, #0x10]          // Guardar los 32 bits de X1 en memoria
-ldurb X3, [X2, #0x10]         // Cargar solo los primeros 8 bits (0xAB)
-hlt 0

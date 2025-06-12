e781017fa41aedb7f2bfe90d50f0fe57eecce81f
Isivaleriano <ivaleriano@udesa.edu.ar>
2025-06-12 16:02:51 -0300

    hice cambios

--- TP5-ThreadPool/src/thread-pool.cc
+++ TP5-ThreadPool/src/thread-pool.cc
@@ -1,17 +1,96 @@
-/**
- * File: thread-pool.cc
- * --------------------
- * Presents the implementation of the ThreadPool class.
- */
-
 #include "thread-pool.h"
 using namespace std;
 
-ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false) {}
+ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false) {
+    for (size_t i = 0; i < numThreads; ++i) {
+        wts[i].ts = thread([this, i] { worker(i); });
+    }
+    dt = thread([this] { dispatcher(); });
+}
 
+void ThreadPool::schedule(const function<void(void)>& thunk) {
+    {
+        lock_guard<mutex> lock(queueLock);
+        taskQueue.push(thunk);
+    }
+    {
+        lock_guard<mutex> lock(tasksLock);
+        tasksInFlight++;
+    }
+    taskAvailable.signal(); 
+}
 
-void ThreadPool::schedule(const function<void(void)>& thunk) {}
+void ThreadPool::dispatcher() {
+    while (true) {
+        taskAvailable.wait(); 
+        if (done) break;
 
-void ThreadPool::wait() {}
+        function<void(void)> task;
+        {
+            lock_guard<mutex> lock(queueLock);
+            if (taskQueue.empty()) continue;
+            task = taskQueue.front();
+            taskQueue.pop();
+        }
 
-ThreadPool::~ThreadPool() {}
+        while (true) {
+            for (auto& w : wts) {
+                lock_guard<mutex> guard(w.mutex_worker);
+                if (w.available) {
+                    w.available = false;
+                    w.thunk = task;
+                    w.ready.signal();
+                    goto assigned;
+                }
+            }
+            this_thread::yield();
+        }
+    assigned:
+        ;
+    }
+}
+
+void ThreadPool::worker(int id) {
+    while (true) {
+        wts[id].ready.wait();
+        if (done) break;
+
+        wts[id].thunk(); 
+
+        {
+            lock_guard<mutex> guard(wts[id].mutex_worker);
+            wts[id].available = true;
+        }
+
+        bool notify = false;
+        {
+            lock_guard<mutex> lock(tasksLock);
+            tasksInFlight--;
+            if (tasksInFlight == 0) notify = true;
+        }
+
+        if (notify) allDone.signal();
+    }
+}
+
+void ThreadPool::wait() {
+    bool shouldWait = false;
+    {
+        lock_guard<mutex> lock(tasksLock);
+        shouldWait = (tasksInFlight > 0);
+    }
+    if (shouldWait) allDone.wait();
+}
+
+ThreadPool::~ThreadPool() {
+    wait();        
+    done = true;   
+
+    taskAvailable.signal(); 
+    for (auto& w : wts) w.ready.signal(); 
+
+    if (dt.joinable()) dt.join();
+    for (auto& w : wts) {
+        if (w.ts.joinable()) w.ts.join();
+    }
+}
